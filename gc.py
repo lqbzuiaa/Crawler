6838fed0acec2e60b554fcf471c4da8f9c1506b2
lqbzuiaa <gaoye456@sina.com>
2016-10-27 22:37:38 +0800

    test

--- gc.py
+++ gc.py
@@ -1,1 +1,25 @@
+import requests
 import os
+import sys
+import webbrowser
+
+def getHtml(url):
+    page = requests.get(url,timeout=20)
+    return page.text
+#
+
+os.system('cls')
+# sys.setdefaultencoding('utf-8')
+url = "https://xue.glgoo.com/scholar?hl=zh-CN&q=smooth+particle+hydrodynamic"
+# html = getHtml(url)
+# webbrowser.register
+# webbrowser.open(url)
+
+# data = webbrowser.get()
+# print(data)
+help(webbrowser)
+
+
+# filePoint = open('test.txt','w',encoding='utf-8')
+# filePoint.write(data)
+# filePoint.close()
