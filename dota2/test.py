5fe5476d8c6257cafa0bdcdbb419873ff65dcd1a
lqbzuiaa <gaoye456@sina.com>
2017-04-05 15:53:41 +0800

    fu

--- dota2/test.py
+++ dota2/test.py
@@ -1,2 +1,11 @@
-from os import *
-print('haha')
+import os
+import requests
+
+r = requests.get('http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/', 
+params = {'format':'XML',
+'key':'A2378FD5FA20FDC79CC0BAA78675F631',
+'skill':'1',
+'min_players':'10',
+'league_id':'0',
+'start_at_match_id':'3099116571'})
+print(r.text)
